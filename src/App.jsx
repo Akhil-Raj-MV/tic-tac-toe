9a1cb166bc8c0d8df1c1595d2a063816ec0e3c6a
Akhil <108akhilraj@gmail.com>
2022-05-23 23:45:38 +0530

    tic tac toe main logic js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React,{useState} from "react";
 import Board from "./components/Board";
 import "./styles/root.scss";
+import { calculateWinner } from "./helpers";
 
 
 const App=()=>{
+  const [board,setBoard]=useState(Array(9).fill(null));
+  const [isXNext,setisXNext]=useState(false);
+  const winner =calculateWinner(board);
+  const message= (winner)?`winner is ${winner}`:`Next player is ${isXNext?'X':'O'}`;
+
+
+  const onClickHandler=(position)=>{
+    if(board[position] || winner) return;
+      setBoard((prev)=>{
+        return(
+          prev.map((square,pos)=>{
+              if(pos===position){
+                return (isXNext)?'X':'O';
+              }
+              else{
+                return square;
+              }
+          })
+        )
+      })
+
+      setisXNext((prev)=>{
+        return !prev;
+      })
+  }
+
   return(
   <div className="app">
     <h1>TIC TAC TOE</h1>
-    <Board/>
+    <h2>Message:{message}</h2>
+    <Board board={board} onClickHandler={onClickHandler}/>
   </div>
   )
 }
